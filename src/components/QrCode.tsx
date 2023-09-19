7029763aca5ec3001964357d1ea5bf268fea7388
iimyzf <agnaou004@gmail.com>
2023-09-19 12:27:32 +0100

    fchkeeeeel

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -74,7 +74,7 @@ const QrCode = ({ toggleQrCode }: QrCodeProps) => {
             <div className="overlay">
                 <div className="pop-up-container">
                     <div className="flex justify-center items-center relative">
-                        <div className="add-channel w-[30em] h-[60vh] max-sm:w-[30vw] max-md:w-[30vw] max-lg:w-[30vw] text-white font-satoshi flex justify-center items-center overflow-y-scroll no-scrollbar overflow-hidden py-[2vw] max-sm:py-[4vw] max-md:py-[4vw]">
+                        <div className="add-channel w-[50em] h-[65vh] max-sm:w-[40vw] max-md:w-[40vw] max-lg:w-[40vw] max-xl:w-[40vw] max-2xl:w-[40vw] text-white font-satoshi flex justify-center items-center overflow-y-scroll no-scrollbar overflow-hidden py-[2vw] max-sm:py-[4vw] max-md:py-[4vw]">
                             <div className="qr-code-container mt-[2vw]">
                                 <div className="qr-code">
                                     <div className="flex flex-col justify-between items-center gap-[1.4vw]">
