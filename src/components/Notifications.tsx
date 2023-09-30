cbeda063a61b2b9dbbbbff4d73095d266b6f8b7f
Abderrahim <bahimzabir@gmail.com>
2023-09-30 17:45:45 +0100

    notifications done

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,5 +1,15 @@
 import { Link } from "react-router-dom";
 import Apollo from "../assets/Apollo.jpg";
+import axios from "axios";
+
+const readNotification = async (id: number) => {
+  const data = {
+    id: [id],
+  };
+  await axios.post("http://localhost:3000/users/readnotification", data, {
+    withCredentials: true,
+  });
+};
 
 const NotificationCount = ({ count }: { count: number }) => {
   if (count === 0) return null;
@@ -11,39 +21,54 @@ const NotificationCount = ({ count }: { count: number }) => {
 };
 
 type NotificationProps = {
-    id : number,
-    createdAt : string,
-    updatedAt : string,
-    userId : number,
-    from : number,
-    type : string,
-    roomid : number,
-    message : string,
-    photo : string,
-    username : string,
-    read : boolean
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  userId: number;
+  from: number;
+  type: string;
+  roomid: number;
+  message: string;
+  photo: string;
+  username: string;
+  read: boolean;
 };
 
-const Notifications = ({ notifications }: { notifications: NotificationProps[] }) => {
+const Notifications = ({
+  notifications,
+}: {
+  notifications: NotificationProps[];
+}) => {
+  const unreadNotifications = notifications.filter(
+    (notification) => notification.read === false
+  );
   return (
     <>
-      <NotificationCount count={notifications.length} />
+      <NotificationCount count={unreadNotifications.length} />
       <div className="box notification-box overflow-y-scroll no-scrollbar w-[20vw]">
         <div className="display">
           <div className="cont">
-            <div className="container-1 m-[.6vw] p-[.5vw] flex justify-center items-center">
-              <Link to="/chat">
-                <div className="flex justify-between items-center gap-[.6vw] max-sm:gap-[2vw] max-md:gap-[2vw] max-lg:gap-[2vw]">
-                  <img
-                    className="w-[2.5vw] h-[2.5vw] max-sm:w-[7vw] max-sm:h-[7vw] max-md:w-[4vw] max-md:h-[4vw] max-lg:w-[4vw] max-lg:h-[4vw] rounded-full"
-                    src={Apollo}
-                  />
-                  <p className="font-satoshi font-normal text-[.8vw] max-sm:text-[1vh] max-md:text-[1.1vh] max-lg:text-[1.1vh]">
-                    mamella sent you a friend request
-                  </p>
-                </div>
-              </Link>
-            </div>
+            {unreadNotifications.map((notification) => (
+              <div
+                key={notification.id}
+                className="container-1 m-[.6vw] p-[.5vw] flex justify-center items-center"
+              >
+                <Link
+                  to={`/view-profile?id=${notification.from}`}
+                  onClick={() => readNotification(notification.id)}
+                >
+                  <div className="flex justify-between items-center gap-[.6vw] max-sm:gap-[2vw] max-md:gap-[2vw] max-lg:gap-[2vw]">
+                    <img
+                      className="w-[2.5vw] h-[2.5vw] max-sm:w-[7vw] max-sm:h-[7vw] max-md:w-[4vw] max-md:h-[4vw] max-lg:w-[4vw] max-lg:h-[4vw] rounded-full"
+                      src={notification.photo}
+                    />
+                    <p className="font-satoshi font-normal text-[.8vw] max-sm:text-[1vh] max-md:text-[1.1vh] max-lg:text-[1.1vh]">
+                      {notification.message}
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
       </div>
